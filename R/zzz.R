d841b960698705cb660dcfed47710163c12b8eda
JT McCrone <mccrone@umich.edu>
2018-02-13 19:42:52 -0500

    Adding ggplot theme attatchment for figures

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
-.onAttach <- function(pkgname, libname) {
+.onAttach <- function(libname,pkgname) {
   my_palette <- wesanderson::wes_palette("Zissou")
   my_theme <- ggplot2::theme_classic()+ ggplot2::theme(text=ggplot2::element_text(family="Arial",size = 18))
   ggplot2::theme_set(my_theme)
 
-  assign("scale_colour_discrete", function(..., values = my_palette) scale_colour_manual(..., values = values), globalenv())
-  assign("scale_fill_discrete", function(..., values = my_palette) scale_fill_manual(..., values = values), globalenv())
+  assign("scale_colour_discrete", function(..., values = my_palette) ggplot2::scale_colour_manual(..., values = values), globalenv())
+  assign("scale_fill_discrete", function(..., values = my_palette) ggplot2::scale_fill_manual(..., values = values), globalenv())
 }
